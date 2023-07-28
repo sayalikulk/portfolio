c091aa7130bd759a7401002412b40a8a61623b68
sayali <sayalikulkarni79@gmail.com>
2023-07-29 00:34:53 +0530

    Added Contact Card

--- src/components/LeftConstant.jsx
+++ src/components/LeftConstant.jsx
@@ -68,6 +68,16 @@ const LeftConstant = () => {
           </svg>
           </a>
         </li>
+        <li class="mr-5 text-xs"><a class="block hover:text-slate-200" href="https://www.linkedin.com/in/bchiang7/" target="_blank" rel="noreferrer">
+          <span class="sr-only">LinkedIn</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#01e2c7" class="w-6 h-6">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+          </svg>
+
+
+          </a>
+        </li>
         </ul>
     </div>
 // </header>
