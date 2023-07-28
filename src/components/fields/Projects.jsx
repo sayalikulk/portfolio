19de3e15e8060a646f171af495b928ca2a90e2e6
sayali <sayalikulkarni79@gmail.com>
2023-07-29 00:10:55 +0530

    Fixed margins

--- src/components/fields/Projects.jsx
+++ src/components/fields/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectCard from '../utils/ProjectCard'
 const Projects = () => {
   return (
   <div id="projects" className='p-[50px] md:pt-[100px] md:pl-[100px]'>
-  <h2 class="font-bold uppercase pb-[20px] text-[#01e2c7]">Projects</h2>   
+  <h2 class="font-bold uppercase pb-[40px] text-[#01e2c7]">Projects</h2>   
   <ProjectCard project_title="Project Title" project_description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil." tech_used={["React", "Java", "C++"]}/>
 
     
